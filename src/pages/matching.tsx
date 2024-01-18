7f154ef7943ce0aaa43408bcb84f4dca4e470479
unknown <febrilanianurislami@gmail.com>
2024-01-18 14:24:41 +0700

    update matching card

--- src/pages/matching.tsx
+++ src/pages/matching.tsx
@@ -12,9 +12,6 @@ const Matching: React.FC = () => {
   const [animalCards, setAnimalCards] = useState<AnimalCard[]>([]);
   const [flippedCards, setFlippedCards] = useState<number[]>([]);
   const [matchedPairs, setMatchedPairs] = useState<number>(0);
-  const [timer, setTimer] = useState<number>(300); // 5 menit dalam detik
-  const [isGameOver, setIsGameOver] = useState<boolean>(false);
-  const [isGameStarted, setIsGameStarted] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchAnimalImages = async () => {
@@ -24,7 +21,9 @@ const Matching: React.FC = () => {
         );
         const dogImages = response.data.message;
 
+        // Duplicating the array to create pairs of matching cards
         const duplicatedImages = [...dogImages, ...dogImages];
+
         const shuffledImages = duplicatedImages.sort(() => Math.random() - 0.5);
 
         const initialAnimalCards: AnimalCard[] = shuffledImages.map(
@@ -45,8 +44,6 @@ const Matching: React.FC = () => {
   }, []);
 
   const handleCardClick = (id: number) => {
-    if (isGameOver || !isGameStarted) return; // Jika permainan belum dimulai atau sudah selesai, abaikan klik
-
     const clickedCard = animalCards.find((card) => card.id === id);
 
     if (clickedCard && !clickedCard.isFlipped && flippedCards.length < 2) {
@@ -69,18 +66,17 @@ const Matching: React.FC = () => {
       const [firstCardId, secondCardId] = flippedCards;
 
       if (animalCards[firstCardId].image === animalCards[secondCardId].image) {
-        // Matched! Keep cards face up
         setMatchedPairs((prevPairs) => prevPairs + 1);
         setFlippedCards([]);
       } else {
-        // Not matched. Flip cards back after a delay
         setTimeout(() => {
-          const updatedCards = animalCards.map((card) =>
-            flippedCards.includes(card.id)
-              ? { ...card, isFlipped: false }
-              : card
+          setAnimalCards((prevCards) =>
+            prevCards.map((card) =>
+              flippedCards.includes(card.id)
+                ? { ...card, isFlipped: false }
+                : card
+            )
           );
-          setAnimalCards(updatedCards);
           setFlippedCards([]);
         }, 1000);
       }
@@ -97,46 +93,24 @@ const Matching: React.FC = () => {
     }
   }, [flippedCards]);
 
-  useEffect(() => {
-    if (matchedPairs === animalCards.length / 2) {
-      // Semua pasangan telah ditemukan, hentikan permainan
-      setIsGameOver(true);
-    }
-  }, [matchedPairs, animalCards]);
-
-  useEffect(() => {
-    let interval: number | undefined;
-
-    if (timer > 0 && !isGameOver && isGameStarted) {
-      interval = window.setInterval(() => {
-        setTimer((prevTimer) => prevTimer - 1);
-      }, 1000);
-
-      return () => clearInterval(interval);
-    } else if (timer === 0) {
-      // Waktu habis, hentikan permainan
-      setIsGameOver(true);
-      if (interval) {
-        clearInterval(interval);
-      }
-    }
-  }, [timer, isGameOver, isGameStarted]);
-
-  const startGame = () => {
-    setIsGameStarted(true);
-  };
-
   const resetGame = () => {
-    setIsGameOver(false);
-    setIsGameStarted(false);
-    setMatchedPairs(0);
-    setTimer(300); // 5 menit dalam detik
+    // Reset state dan ambil gambar baru
     setFlippedCards([]);
-    const shuffledImages = animalCards.map((card) => ({
-      ...card,
-      isFlipped: false,
-    }));
-    setAnimalCards(shuffledImages.sort(() => Math.random() - 0.5));
+    setMatchedPairs(0);
+
+    const shuffledImages = animalCards
+      .map((card) => card.image)
+      .sort(() => Math.random() - 0.5);
+
+    const resetAnimalCards: AnimalCard[] = shuffledImages.map(
+      (image: string, index: number) => ({
+        id: index,
+        image,
+        isFlipped: false,
+      })
+    );
+
+    setAnimalCards(resetAnimalCards);
   };
 
   return (
@@ -146,12 +120,11 @@ const Matching: React.FC = () => {
         <h1 className="text-xl font-bold font-poppins text-center text-purple-900 my-5">
           MATCHING CARD
         </h1>
-
-        <div className="mx-auto w-full py-5 md:w-1/2 grid grid-cols-4 gap-2 justify-items-center bg-purple-200 shadow-md shadow-purple-900 border border-purple-900">
+        <div className="mx-auto w-full p-5 md:w-1/2 grid grid-cols-4 gap-2 justify-items-center bg-purple-200 shadow-md shadow-purple-900 border border-purple-900">
           {animalCards.map((card) => (
             <div
               key={card.id}
-              className={`w-14 h-14 md:w-24 md:h-24 border border-purple-900 bg-white flex items-center cursor-pointer`}
+              className={`md:w-20 md:h-20 w-14 h-14 border border-purple-900 bg-white flex items-center cursor-pointer`}
               onClick={() => handleCardClick(card.id)}
             >
               {card.isFlipped ? (
@@ -168,16 +141,6 @@ const Matching: React.FC = () => {
         </div>
         <div className="text-center mt-4">
           <p>Matched Pairs: {matchedPairs}</p>
-          <p>Time Left: {timer} seconds</p>
-          {isGameOver && <p>Game Over!</p>}
-          {!isGameStarted && (
-            <button
-              className="bg-green-500 rounded-md text-white p-2 my-2 mr-2"
-              onClick={startGame}
-            >
-              Start Game
-            </button>
-          )}
           <button
             className="bg-purple-500 rounded-md text-white p-2 my-2 ml-2"
             onClick={resetGame}
